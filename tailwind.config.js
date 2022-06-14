502e58135d9fbcff63b100b5d8a75b3f5b0225f1
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2022-06-14 09:00:02 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,14 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  theme:{
+    colors:{
+      'blackN' : '000000',
+      'whiteN' : 'FFFFFF',
+      'grey1' : 'F0F0F0',
+      'grey2' : 'CACACA',
+      'orange1' : 'FFC456',
+      'orange2' : 'FFB52C'
+    }
+  }
 }
